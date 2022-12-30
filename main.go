5d17f6eebb021da39ec3d647e4dcabc49e57719c
t-shimpo <zanjibaru7@gmail.com>
2022-12-30 15:50:36 +0900

    DBとの接続を確認する

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/t-shimpo/go-echo-gorm-rest/model"
 )
+
+func connect(c echo.Context) error {
+	db, _ := model.DB.DB()
+	defer db.Close()
+	err := db.Ping()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "DB接続失敗しました")
+	} else {
+		return c.String(http.StatusOK, "DB接続しました")
+	}
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/", connect)
 	e.Logger.Fatal(e.Start(":8080"))
 }
